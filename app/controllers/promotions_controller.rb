1b99a808b2e75523345307c6a6b87de484e61101
leandrohorizon <leandro.mcontato@gmail.com>
2021-03-20 15:08:41 -0300

    Validando promoção

--- app/controllers/promotions_controller.rb
+++ app/controllers/promotions_controller.rb
@@ -13,7 +13,11 @@ class PromotionsController < ApplicationController
 
   def create
     @promotion = Promotion.create(promotion_params)
-    redirect_to @promotion
+    if @promotion.save
+      redirect_to @promotion
+    else
+      render :new
+    end
   end
 
   private
